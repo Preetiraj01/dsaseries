cec94b3a1a1703fbce6e5480bd4839b75b67f9b7
preetiraj01 <preetiraj0110@gmail.com>
2026-04-28 12:40:56 +0530

    pattern1 added

---
+++ pattern1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int p=1;
+    cout<<p<<endl;
+    cout<<"I am PREETI RAJ and I am going to get placed in 2026 only for sure!"<<endl;
+    return 0;
+}
